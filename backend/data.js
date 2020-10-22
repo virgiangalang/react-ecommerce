170bfe925eab5a211172539793e8cb7d5d4087d8
Virgian Galang <virgiangalang281199@gmail.com>
2020-10-22 11:03:01 +0700

    First Commit

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+            {
+            _id:'1',
+            name: 'Slim Shirt',
+            category: 'Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10
+            },
+            {
+            _id:'2',
+            name: 'Formal Shirt',
+            category: 'Shirt',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10
+            },
+            {
+            _id:'3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 8
+            },
+             {
+            _id:'4',
+            name: 'Lingrie Formal',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 8
+            }
+ ]
+}
